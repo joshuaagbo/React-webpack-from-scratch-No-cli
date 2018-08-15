7f3c06a5e3dd9aa44b02489ba0997f7efd71a4fa
gemjosh <therealgemjosh@gmail.com>
2018-08-15 13:53:34 +0100

    new changes

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render() {
+    return(
+      <div className = "App">
+        <h2>Welcome To React&amp;Webpack From Scratch.</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
